8ef60e3fb8d4fea7add5468b9f14936f75830a92
Brad Kendall <me@bradk.xyz>
2018-04-04 08:28:47 +0100

    Remove submodule.  Change marketplace dependency to NPM.

--- tabs/src/app/app.module.ts
+++ tabs/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { MarketplaceAppModule } from 'kittycash-marketplace';
+import { MarketplaceAppModule } from 'kittycash-marketplace-lib';
 import { GameComponent } from "./game/game.component";
 import { ScoreboardService } from "./game/scoreboard.service";
 import { AppComponent } from './app.component';
